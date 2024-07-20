79c3cff2dc38c91f92d42456b6b9466ceda843d1
doctor1 <08armaan21singh@gmail.com>
2024-07-20 11:00:40 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+startx & ./selkies-gstreamer/selkies-gstreamer-run --addr=0.0.0.0 --port=8080 --enable_https=false --https_cert=/etc/ssl/certs/ssl-cert-snakeoil.pem --https_key=/etc/ssl/private/ssl-cert-snakeoil.key --basic_auth_user=user --basic_auth_password=mypasswd --encoder=x264enc --enable_resize=false
